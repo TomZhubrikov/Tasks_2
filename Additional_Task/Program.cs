e984bc44f3e4194099dc55b3e7cbdfc2b3a290d7
Tom <artzhubrikov@mail.ru>
2022-11-28 17:30:27 +0300

    Completed

--- Additional_Task/Program.cs
+++ Additional_Task/Program.cs
@@ -3,4 +3,26 @@
 // то есть элемента, который будет наибольшим, 
 // если из последовательности удалить наибольший элемент.
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter a sequence of integers (еnter 0 to end input):");
+int n = int.Parse(Console.ReadLine());
+int max = n;
+int sec_max = max;
+n = int.Parse(Console.ReadLine());
+    if (n > max) {
+        sec_max = max;
+        max = n;
+    }
+    else {
+        sec_max = n;
+    }
+while (n != 0) {
+    n = int.Parse(Console.ReadLine());
+    if (n > max) {
+        sec_max = max;
+        max = n;
+    }
+    else if (n > sec_max) {
+        sec_max = n;
+    }
+}
+Console.WriteLine($"Second max in the sequence -> {sec_max}");
